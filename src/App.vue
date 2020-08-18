76cb563195d1233c74802351c73fbe2c22ed5e59
Ohien Stephen <ohienstephen@gmail.com>
2020-08-18 03:15:51 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <header>
+      <div class="header-div">
+            <a class="logo">Todo App</a>
+            <div id="nav">
+              <router-link to="/">Home</router-link> |
+              <router-link to="/about">About</router-link>
+            </div>
+      </div>
+    </header>
+    <div id="wrapper">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body{
+  box-sizing: border-box;
+  margin:0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
+header{
+  background-color: rgb(125, 4, 238);
+  color:white;
+  padding: 20px;
+  width: 100%; 
 
-#nav {
-  padding: 30px;
+    .header-div{
+      width: 90%;   
+      margin:0 auto; 
+      display: flex;
+      justify-content: space-between;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+        a{    
+            color:inherit;
+            text-decoration: none;
+        }
+      .logo {
+          font-weight: bold;
+      }
   }
+}
+#wrapper{
+  width:90%;
+  margin: 1em auto;
+  height: 75vh;
+  border: 1px solid gray;
 }
 </style>
